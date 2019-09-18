ac82eedf0621962f10d1b391ccfff763726b1c55
Lukasz Sielski <lukasz.sielski@mailonline.co.uk>
2019-09-18 14:05:00 +0100

    Deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 aws configure set default.s3.max_concurrent_requests 50
-aws s3 sync ./public s3://sielay.com --acl=public-read --delete --profile sielaygod
-aws cloudfront create-invalidation --distribution-id E3A7I7P96TY7U7 --paths "/*"
+aws s3 sync ./public s3://phonekills.co.uk --acl=public-read --delete --profile sielaygod
+aws cloudfront create-invalidation --distribution-id 3K81M7CLNJTYO --paths "/*"
